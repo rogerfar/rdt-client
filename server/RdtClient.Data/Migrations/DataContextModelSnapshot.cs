d817f5fd028805d98e0ffb7677b2aa17703c2c08
Roger Versluis <roger.versluis@fieldcap.ca>
2020-12-12 11:02:54 -0700

    Update packages and update Runtime to .NET 5

--- server/RdtClient.Data/Migrations/DataContextModelSnapshot.cs
+++ server/RdtClient.Data/Migrations/DataContextModelSnapshot.cs
@@ -2,7 +2,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RdtClient.Data.Data;
 
 namespace RdtClient.Data.Migrations
