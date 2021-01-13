b0f584a6d59da77accfd275727db535bea65cb5c
Roger Far <roger@versluis.ca>
2021-01-13 13:46:47 -0700

    no message

--- server/RdtClient.Service/Services/QBittorrent.cs
+++ server/RdtClient.Service/Services/QBittorrent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using RdtClient.Data.Enums;
 using RdtClient.Service.Models.QBittorrent;
 using RdtClient.Service.Models.QBittorrent.QuickType;
 
