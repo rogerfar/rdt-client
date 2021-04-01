c4a7c1e5ed006445c5f661c756cd228a28494bc1
Roger Far <roger@versluis.ca>
2021-04-01 16:31:05 -0600

    Fixed some bugs in the update script

--- server/RdtClient.Web/Update.ps1
+++ server/RdtClient.Web/Update.ps1
@@ -1,14 +1,24 @@
-﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+﻿$currentDirectory = $PSScriptRoot
+
+Write-Host "Starting update script in $currentDirectory"
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {   
     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     Break
 }
 
+Write-Host "Stopping ReadDebridClient..."
+
 Stop-Service RealDebridClient
+
+Write-Host "Stopped ReadDebridClient"
 
 $releasesUri = "https://api.github.com/repos/rogerfar/rdt-client/releases/latest"
 $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like "*.zip").browser_download_url
+
+Write-Host "Downloading $downloadUri"
 
 $pathZip = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath $(Split-Path -Path $downloadUri -Leaf)
 
@@ -16,11 +26,26 @@ Invoke-WebRequest -Uri $downloadUri -Out $pathZip
 
 $tempExtract = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath $((New-Guid).Guid)
 
+Write-Host "Extracting to $tempExtract"
+
 Expand-Archive -Path $pathZip -DestinationPath $tempExtract -Force
-Copy-Item -Path ".\appsettings.json" -Destination $tempExtract -Force 
-Move-Item -Path "$tempExtract\*" -Destination $pathExtract -Force
+
+Write-Host "Backing up appsettings.json"
+
+Copy-Item -Path "$currentDirectory\appsettings.json" -Destination $tempExtract -Force 
+
+Write-Host "Moving new files"
+
+Move-Item -Path "$tempExtract\*" -Destination $currentDirectory -Force
+
+Write-Host "Removing temp files"
+
 Remove-Item -Path $tempExtract -Force -Recurse -ErrorAction SilentlyContinue
 
 Remove-Item $pathZip -Force
 
+Write-Host "Starting ReadDebridClient..."
+
 Start-Service RealDebridClient
+
+Write-Host "Started ReadDebridClient"
