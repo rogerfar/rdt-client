2e8ed68ba50bc615d00df7156bec8dbc64045d24
Shawn Anderson <sanderson@eye-catcher.com>
2023-03-17 16:25:37 -0400

    Cleanup

--- docker-build-dev.ps1
+++ docker-build-dev.ps1
@@ -20,10 +20,11 @@ docker rm rdtclientdev
 Write-Host "Building Container"
 $dockerArgs = @( "build", "--force-rm", "--tag", "rdtclientdev", "--progress=$BuildProgress", "--file", $Dockerfile, "." )
 if ($SkipCache.IsPresent) { $dockerArgs += @("--no-cache" ) }
+
 & docker $dockerArgs
 
 Write-Host "Starting Container"
-docker run --cap-add=NET_ADMIN -d -v $downloadPath -v $dbPath --log-driver json-file --log-opt max-size=10m -p 6500:6500 --name rdtclientdev rdtclientdev
+docker run --cap-add=NET_ADMIN -d -v $($downloadPath):/data/downloads -v $($dbPath):/data/db --log-driver json-file --log-opt max-size=10m -p 6500:6500 --name rdtclientdev rdtclientdev
 
 if ($AutoAttach.IsPresent) {
     docker exec -it rdtclientdev /bin/bash
