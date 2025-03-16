e6f7db07630b7e952bf35cbce7a6e3885435ebce
Cucumberrbob <128094686+Cucumberrbob@users.noreply.github.com>
2025-03-16 15:49:46 +0000

    fix merge

--- server/RdtClient.Service/Services/TorrentClients/TorBoxTorrentClient.cs
+++ server/RdtClient.Service/Services/TorrentClients/TorBoxTorrentClient.cs
@@ -4,7 +4,6 @@ using Newtonsoft.Json;
 using TorBoxNET;
 using RdtClient.Data.Enums;
 using RdtClient.Data.Models.TorrentClient;
-using System.Web;
 using RdtClient.Data.Models.Data;
 using RdtClient.Service.Helpers;
 
