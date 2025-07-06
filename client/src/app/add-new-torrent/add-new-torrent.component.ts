30b9b9a4adc8cbd41cb8715c2ca41a31f5c5b6fc
Cucumberrbob <128094686+Cucumberrbob@users.noreply.github.com>
2025-07-06 22:08:28 +0100

    run prettier

--- client/src/app/add-new-torrent/add-new-torrent.component.ts
+++ client/src/app/add-new-torrent/add-new-torrent.component.ts
@@ -50,11 +50,11 @@ export class AddNewTorrentComponent implements OnInit {
     private router: Router,
     private torrentService: TorrentService,
     private settingsService: SettingsService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(params => {
+    this.activatedRoute.queryParams.subscribe((params) => {
       if (params['magnet']) {
         this.magnetLink = decodeURIComponent(params['magnet']);
       }
@@ -70,7 +70,7 @@ export class AddNewTorrentComponent implements OnInit {
       this.downloadAction =
         settings.first((m) => m.key === 'Gui:Default:OnlyDownloadAvailableFiles')?.value === true ? 1 : 0;
       this.finishedAction = settings.first((m) => m.key === 'Gui:Default:FinishedAction')?.value as number;
-      this.finishedActionDelay = settings.first(m => m.key =="Gui:Default:FinishedActionDelay")?.value as number;
+      this.finishedActionDelay = settings.first((m) => m.key == 'Gui:Default:FinishedActionDelay')?.value as number;
       this.downloadMinSize = settings.first((m) => m.key === 'Gui:Default:MinFileSize')?.value as number;
       this.includeRegex = settings.first((m) => m.key === 'Gui:Default:IncludeRegex')?.value as string;
       this.excludeRegex = settings.first((m) => m.key === 'Gui:Default:ExcludeRegex')?.value as string;
