8c7c384bceb2c2af02e8b4239531677b15191848
Roger Far <roger@versluis.ca>
2026-04-27 21:18:33 -0600

    Cleanup.

--- client/src/app/settings/settings.component.ts
+++ client/src/app/settings/settings.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { SettingsService } from 'src/app/settings.service';
 import { Setting } from '../models/setting.model';
 import { NgClass, KeyValuePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Nl2BrPipe } from '../nl2br.pipe';
 import { FileSizePipe } from '../filesize.pipe';
+import { SettingsService } from '../settings.service';
 
 @Component({
   selector: 'app-settings',
